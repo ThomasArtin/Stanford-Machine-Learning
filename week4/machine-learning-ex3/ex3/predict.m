c36ad9baa552e0e7feccdfe4ffb96fd22e097f45
Thomas Joseph <1600440@eng.asu.edu.eg>
2020-08-05 23:59:24 +0200

    Add files via upload

--- week4/machine-learning-ex3/ex3/predict.m
+++ week4/machine-learning-ex3/ex3/predict.m
@@ -6,7 +6,7 @@ function p = predict(Theta1, Theta2, X)
 % Useful values
 m = size(X, 1);
 num_labels = size(Theta2, 1);
-
+n = size(X, 2);
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
@@ -19,13 +19,15 @@ p = zeros(size(X, 1), 1);
 %       function can also return the index of the max element, for more
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
-%
+% 
 
-
-
-
-
-
+a1        =  [ones(m,1)  X];
+z2        =  a1*  Theta1';
+a2        =  sigmoid (z2);
+a2        =  [ones(size(a2,1),1) a2];
+z3        =  a2* Theta2';
+a3        =  sigmoid (z3);
+[max,p]   =  max(a3,[],2);
 
 
 
